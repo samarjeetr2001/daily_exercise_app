d26147bd7d8fe79758e729e5e755ad8836525ca1
samarjeetr2001 <61595281+samarjeetr2001@users.noreply.github.com>
2020-08-20 01:40:53 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 Icon(Icons.line_weight,color: Colors.grey,),
-                Text("Weight")
+                Text("Weights")
               ],
             ),
              Column(
